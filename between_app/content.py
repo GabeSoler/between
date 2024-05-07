7ddd8b17d724e7816f2830612fcd9c9ad369c047
Gabriel Soler <gsoler@gmail.com>
2024-05-07 23:04:49 +0100

    I added images and made it more easthetical in general

--- between_app/content.py
+++ between_app/content.py
@@ -40,24 +40,32 @@ class Content():
 
     path = {
         "dreamer" : {
+        "image" : "between_app/images/dreamer.jpg",
+
         "description" : "The Dreamer path is an invitation towards the magic of poetry, metaphor, art, and daydreaming. This path feels deeply and believes dream brings deeper truths.",
         "shadow" : "When we polarise into it we may find ourselves forgeting the concrete world over fantasy.",
         "traditions" : "Jungian analysis, Psychoanalysis.",
         "authors" : "Jung"
     },
         "sage"  : {
+        "image" : "between_app/images/sage.jpg",
+
         "description" : "The Sage path invite us to analyse things in their components and to create more encompasing models that help us clarify our minds and the big problems of existence.",
         "shadow" : "When we polarise into it we may become obsessed with concepts and with the need to categorise every inch of human experience, clauding our mind to the beauty of life.",
         "traditions" : "Integral approach, Integrative approaches.",
         "authors" : "Wilber, Kegan"
     },
         "carer" : {
+        "image" : "between_app/images/carer.jpg",
+
         "description" : "The Carer path is an invitation towards closeness to otherness, by an appreciation of life and the movements of nature.",
         "shadow" : "When we polarise into it we may become too close to care for others forgeting to care for ouselves, we become submisive.",
         "traditions" : "Person Centred, Focusing, Relational Psychoanalisis.",
         "authors" : "Rogers, Orange"
     },
         "explorer" : {
+        "image" : "between_app/images/explorer.jpg",
+
         "description" : "The Explorer path is an invitation to keep our curiosity for novelty, for new tecniques and solutions, and to not stop looking for something better.",
         "shadow" : "When we polarise into it we may become extremely passionate of our serch and we may lose the natural balance, risking manic defenses.",
         "traditions" : "Evidence Based, Cognitive Behavioural",
@@ -67,6 +75,8 @@ class Content():
 
     tradition = {
         "artist" : {
+        "image" : "between_app/images/artist.jpg",
+
         "description" : "The Artist has a self deepening approach, focussing more in their own feelings and experiences, aiming to explore the deepths of the soul.",
         "shadow" : "When we polarise into it we may have difficulties with stopping our delightful practices, risking addiction and fixation.",
         "traditions" : "Jungian analysis, Psychoanalysis(Bion, Klain, Winnicott), Psychodelic treatements, holotropic breathing.",
@@ -74,18 +84,24 @@ class Content():
         },
 
         "warrior" : {
+        "image" : "between_app/images/warrior.jpg",
+
         "description" : "The Warrior has a self development approach, aiming to help by becoming stronger, achieving higher concepts and techniques.",
         "shadow" : "When we polarise into it we risk to develop a tunel vision towards our fight and projects, not able to see around anymore.",
         "traditions" : "Cognitive Behavioural, Gestalt",
         "authors" : "Beck, Ellis, Perls, Wilber"
     },
-        "lider" : {
-        "description" : "The Lider has a group approach where they want to find an inspiration that may take them and others to a better place.",
+        "leader" : {
+        "image" : "between_app/images/leader.jpg",
+
+        "description" : "The Leader has a group approach where they want to find an inspiration that may take them and others to a better place.",
         "shadow" : "When we polarise into it we may become too embedded in our group ideals and develop a cult like behaviour, rejecting external opinions.",
         "traditions" : "Confesional therapies",
         "authors" : "Freud when making seven rings"
         },
         "coach" : {
+        "image" : "between_app/images/coach.jpg",
+
         "description" : "The Coach position works with a team, and aims for its perfecting by following clear techniques and strategies.",
         "shadow" : "When we polarise into it we may create a relatiionship where one dominates over the rest because they may 'know more'.",
         "traditions" : "Cognitive Behavioural.",
