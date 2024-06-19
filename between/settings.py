91a369b198e7b43c7205bb2c6823e5a8d5bd55b6
Gabriel Soler <gsoler@gmail.com>
2024-06-19 13:36:01 +0100

    re:test: pointed test to current database. risk is that creates and destroyes info and some think its dangerous

--- between/settings.py
+++ between/settings.py
@@ -135,8 +135,11 @@ else:
         }
 
 TEST_DATABASES = {        
-    "ENGINE": "django.db.backends.sqlite3",
-    "NAME": BASE_DIR / "db.sqlite3",
+        "default": dj_database_url.config(
+            env="DATABASE_URL",
+            conn_max_age=600,
+            conn_health_checks=True,
+            ssl_require=True,)
 }
 
 # Password validation
