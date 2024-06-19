296de71347d8bc652191b917d99e3c90b2bebffb
Gabriel Soler <gsoler@gmail.com>
2024-06-19 13:46:58 +0100

    re test:pointed to sqlite

--- between/settings.py
+++ between/settings.py
@@ -118,12 +118,10 @@ if IS_HEROKU_APP:
             conn_health_checks=True,
             ssl_require=True,
         ),
-        "test": dj_database_url.config(
-            env="DATABASE_URL",
-            conn_max_age=600,
-            conn_health_checks=True,
-            ssl_require=True,
-        )
+        "test": 
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        
     } 
 
 else:
