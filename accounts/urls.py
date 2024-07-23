5f1411efaaad4c1a3b3b859dbb5c13eab51a8198
Gabriel Soler <gsoler@gmail.com>
2024-07-23 22:44:24 +0100

    added allauth - base for social accounts, I need to link it in the sites (google, apple) - templating system, added its profile section (needs more work to link and shape). I would like to add a profile part for the community and app management.

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,7 @@
 
 from django.urls import path,include
 from .views import settings_view,community_profile_view
+from django.views.generic.base import TemplateView
 
 app_name = 'accounts'
 
@@ -10,5 +11,8 @@ urlpatterns = [
     #path('',include('django.contrib.auth.urls')),
 
     path('settings/',settings_view,name='settings'),
-    path('profile/',community_profile_view,name='account_profile'),
+    #path('profile/',community_profile_view,name='account_profile'),
+    path("profile/", TemplateView.as_view(template_name="profile.html")), #using allauth system, to integrate a account centre
+                                                                        #i need to add link in the manage.py file and extend it too
+
 ]
--- between/settings.py
+++ between/settings.py
@@ -54,6 +54,10 @@ INSTALLED_APPS = [
     'django_bootstrap5', #css framework
     'allauth',
     'allauth.account', #authorisations
+    'allauth.socialaccount', #the social account sytem 
+    'allauth.socialaccount.providers.apple', #apple social account
+    'allauth.socialaccount.providers.google', #google social account
+
     "whitenoise.runserver_nostatic", #white noise, static files manager
 
 #django apps added
@@ -234,55 +238,6 @@ BOOTSTRAP5 = {
 
     # The complete URL to the Bootstrap CSS theme file (None means no theme).
     "theme_url": "https://bootswatch.com/5/sandstone/bootstrap.css",
-
-    # Color mode (None means do not set color mode).
-    "color_mode": None,
-
-    # Put JavaScript in the HEAD section of the HTML document (only relevant if you use bootstrap5.html).
-    'javascript_in_head': False,
-
-    # Wrapper class for non-inline fields.
-    # The default value "mb-3" is the spacing as used by Bootstrap 5 example code.
-    'wrapper_class': 'mb-3',
-
-    # Wrapper class for inline fields.
-    # The default value is empty, as Bootstrap5 example code doesn't use a wrapper class.
-    'inline_wrapper_class': '',
-
-    # Label class to use in horizontal forms.
-    'horizontal_label_class': 'col-sm-2',
-
-    # Field class to use in horizontal forms.
-    'horizontal_field_class': 'col-sm-10',
-
-    # Field class used for horizontal fields withut a label.
-    'horizontal_field_offset_class': 'offset-sm-2',
-
-    # Set placeholder attributes to label if no placeholder is provided.
-    'set_placeholder': True,
-
-    # Class to indicate required field (better to set this in your Django form).
-    'required_css_class': '',
-
-    # Class to indicate field has one or more errors (better to set this in your Django form).
-    'error_css_class': '',
-
-    # Class to indicate success, meaning the field has valid input (better to set this in your Django form).
-    'success_css_class': '',
-
-    # Enable or disable Bootstrap 5 server side validation classes (separate from the indicator classes above).
-    'server_side_validation': True,
-
-    # Renderers (only set these if you have studied the source and understand the inner workings).
-    'formset_renderers':{
-        'default': 'django_bootstrap5.renderers.FormsetRenderer',
-    },
-    'form_renderers': {
-        'default': 'django_bootstrap5.renderers.FormRenderer',
-    },
-    'field_renderers': {
-        'default': 'django_bootstrap5.renderers.FieldRenderer',
-    },
 }
 
 
@@ -311,3 +266,4 @@ AUTHENTICATION_BACKENDS = [
     # `allauth` specific authentication methods, such as login by email
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
+
