aef52ff8b020d9fe390365159f98b745667749c4
Gabriel Soler <gsoler@gmail.com>
2024-09-25 17:39:50 +0100

    refactored functions into one

--- accounts/apps.py
+++ accounts/apps.py
@@ -18,36 +18,18 @@ class AccountsConfig(AppConfig):
                 form = model.objects.get(pk=form_pk)
                 form.user = user
                 form.save()
-                print("form linked to user")
+                print("form linked to user") # For checking your logs
                 request.session['linked'] = "true" #for testing
             except Exception as e:
-                print(f"failed to link user because: {e}")
+                print(f"failed to link user because: {e}") # for checking your logs
 
         @receiver(user_signed_up)
         def user_signed_up(request, user, **kwargs):
             """function to be applied after signup so if there is a test made before it is attached to the user"""
-            try:
-                form_pk = request.session["form_pk"]
-                form_pk = uuid.UUID(form_pk)
-                form = PersonalStyle.objects.get(pk=form_pk)
-                form.user = user
-                form.save()
-                print("form linked to user")
-                request.session['linked'] = "true" #for testing
-            except Exception as e:
-                print(f"failed to link user because: {e}")
+            save_form_after_log(request,user,PersonalStyle,**kwargs)
 
         @receiver(user_logged_in)
         def user_logged_in(request, user, **kwargs):
             """function to be applied after login so if there is a test made before it is attached to the user"""
-            try:
-                form_pk = request.session["form_pk"]
-                form_pk = uuid.UUID(form_pk)
-                form = PersonalStyle.objects.get(pk=form_pk)
-                form.user = user
-                form.save()
-                print("form linked to user")
-                request.session['linked'] = "true" #for testing
-            except Exception as e:
-                print(f"failed to link user because: {e}")
+            save_form_after_log(request,user,PersonalStyle,**kwargs)
         
