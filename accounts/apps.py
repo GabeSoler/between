489bee8b63e8034938e8ef47df9b5185d766bc0b
Gabriel Soler <gsoler@gmail.com>
2024-09-27 16:30:58 +0100

    small changen in name for error key/value in apps.py

--- accounts/apps.py
+++ accounts/apps.py
@@ -22,9 +22,9 @@ class AccountsConfig(AppConfig):
                 print("form linked to user") # For checking your logs
                 request.session['linked'] = "true" #for testing
             except Exception as e:
-                request.session["form_pk"] = "error"
+                request.session["linked"] = "error"
                 error_text = f"failed to link user because: {e}"
-                request.session["form_pk_error"] = error_text
+                request.session["linked_error"] = error_text
                 print(error_text) # for checking your logs
 
         @receiver(user_signed_up)
