27c7bd88d53f5ccb1fe6844f93a6557816ee4069
Gabriel Soler <gsoler@gmail.com>
2024-10-02 20:44:41 +0100

    updated the signal function to catch errors instead of activate with an if (that was not working) also added a text at the end of front page for sign up

--- accounts/apps.py
+++ accounts/apps.py
@@ -18,9 +18,9 @@ class AccountsConfig(AppConfig):
 
         def save_form_after_log(request,user,model,**kwargs)->str:
             """Base function for adding pk to form after auth"""
-            form_pk = request.session['form_pk']
-            form_pk = uuid.UUID(form_pk)
             try:
+                form_pk = request.session['form_pk']
+                form_pk = uuid.UUID(form_pk)
                 save_user(model,form_pk,user)
             except Exception as e:
                 error_text = f"failed to link user because: {e}"
@@ -32,12 +32,10 @@ class AccountsConfig(AppConfig):
         @receiver(user_signed_up)
         def user_signed_up(request, user, **kwargs):
             """function to be applied after signup so if there is a test made before it is attached to the user"""
-            if request.session["form_pk"]:
-                save_form_after_log(request,user,PersonalStyle,**kwargs)
+            save_form_after_log(request,user,PersonalStyle,**kwargs)
 
         @receiver(user_logged_in)
         def user_logged_in(request, user, **kwargs):
             """function to be applied after login so if there is a test made before it is attached to the user"""
-            if request.session["form_pk"]:  
-                save_form_after_log(request,user,PersonalStyle,**kwargs)
+            save_form_after_log(request,user,PersonalStyle,**kwargs)
         
