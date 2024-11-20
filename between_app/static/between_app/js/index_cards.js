7b8771b5963fa7871fe55fb2319fb621b650acda
Gabriel Soler <gsoler@gmail.com>
2024-11-20 20:47:43 +0000

    fine tunning javascript animations for index page

--- between_app/static/between_app/js/index_cards.js
+++ between_app/static/between_app/js/index_cards.js
@@ -6,38 +6,35 @@ $(function(){
     function isInViewport(card) {
         var elementTop = $(card).offset().top;
         var elementBottom = elementTop + $(card).outerHeight();
-        var viewportTop = $(window).scrollTop();
-        var viewportBottom = viewportTop + $(window).height();
+        var viewportTop = $(window).scrollTop()-80;
+        var viewportBottom = viewportTop + $(window).height()+80;
         return elementBottom > viewportTop && elementTop < viewportBottom;
         };
 
 
     //Animate if scrolling and in view port, appearing from right
     $(window).scroll(function() {
-          $(".card").not(":first").each(function(index,item){
+          $(".card").each(function(index,item){
             if (isInViewport(item)) {
               item.animate({
                 "transform":"translateX(0)",
                 "opacity":1
-                },1200+index*100,"ease-out")          
-              } else {
+                },1200+index*200,"ease-out")          
+              }else{
                 this.animate({
                     "transform":"translateX(5%)",
                     "opacity":0
                     },800)    
               };
             });
-            if (isInViewport($("#welcomeText"))){
-                    $("#animateMe1").fadeIn(400);
-                    $("#animateMe2").fadeIn(800);
-                    $("#animateMe3").fadeIn(1000);
+            if (isInViewport($("#alert_bottom"))){
+                $("#alert_bottom_text").fadeIn(800)
             }else{
-                $("#animateMe1").fadeOut();
-                $("#animateMe2").fadeOut();
-                $("#animateMe3").fadeOut();
-            };           
+                $("#alert_bottom_text").fadeOut(100)
+                };           
         });
-    $("#landing-text_1").hide().fadeIn(900);
-    $("#landing-text_2").hide().fadeIn(1200);
-    $("#landing-button").hide().fadeIn(1800);
+    $("#landing-text_1").hide().fadeIn(600);
+    $("#landing-text_2").hide().fadeIn(1000);
+    $("#landing-button").hide().fadeIn(1400);
+    $("#my_site_link").hide().fadeIn(2000);
 })
