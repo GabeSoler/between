852c0e6a61655fa25c63358d59941f9612aff9fb
Gabriel Soler <gsoler@gmail.com>
2024-11-22 22:23:48 +0000

    having an endless loop

--- between_app/static/between_app/js/index_cards.js
+++ between_app/static/between_app/js/index_cards.js
@@ -6,27 +6,50 @@ $(function(){
     function isInViewport(card) {
         var elementTop = $(card).offset().top;
         var elementBottom = elementTop + $(card).outerHeight();
-        var viewportTop = $(window).scrollTop()-80;
-        var viewportBottom = viewportTop + $(window).height()+80;
-        return elementBottom > viewportTop && elementTop < viewportBottom;
+        var viewportTop = $(window).scrollTop();
+        var viewportBottom = viewportTop + $(window).height();
+        return elementBottom > viewportTop + 80 && elementTop < viewportBottom - 80;
+        };
+    function isInFullView(card) {
+        var elementTop = $(card).offset().top;
+        var elementBottom = elementTop + $(card).outerHeight();
+        var viewportTop = $(window).scrollTop();
+        var viewportBottom = viewportTop + $(window).height();
+        return elementBottom > viewportTop + 100 && elementTop < viewportBottom - 100;
+        };
+    function isInBottomNav(element) {
+        let elementTop = $(element).offset().top;
+        let elementBottom = elementTop + $(element).outerHeight();
+        let viewportTop = $(window).scrollTop();
+        let navbarTop = viewportTop + $(window).height() - 50;
+        let navbarBottom = viewportTop + $(window).height();
+        return navbarBottom > elementTop < navbarTop;
         };
 
+    function animate(item) {
+      item.velocity({
+        transform: ["translateX(0)","translateX(5%)"],
+        opacity:[1,0]
+        },
+        {
+          duration: 1200,
+          ease: "ease-out",
+          loop: false
+        });
+
+    }
 
     //Animate if scrolling and in view port, appearing from right
     $(window).scroll(function() {
           $(".card").each(function(index,item){
-            if (isInViewport(item)) {
-              item.animate({
-                "transform":"translateX(0)",
-                "opacity":1
-                },1200+index*200,"ease-out")          
-              }else{
-                this.animate({
-                    "transform":"translateX(5%)",
-                    "opacity":0
-                    },800)    
-              };
+            if (isInFullView(item)) {
+              item.velocity("finish");
+            } else if (isInBottomNav(item)) {
+              item.velocity("fadeInRight");
+            };
+            return;
             });
+
             if (isInViewport($("#alert_bottom"))){
                 $("#alert_bottom_text").fadeIn(800)
             }else{
