c38c5985534a441f6c10f35d41602c1a8261a836
Gabriel Soler <gsoler@gmail.com>
2025-01-24 18:39:16 +0000

    cosmetic changes: Reshufle of icons to represent better areas and make navegation easier Change of url dive to diver

--- between/urls.py
+++ between/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
 
     path('learning/',include('learning_logs.urls')),
     path('techniques/',include('techniques_app.urls')),
-    path('dive/',include('dive_app.urls')),
+    path('diver/',include('dive_app.urls')),
     path('',include('between_app.urls')),
     
     #flat pages config
