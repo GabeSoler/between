61fbbf7f9afedd807d196a118ab8efbbd148ec74
GabeSoler <gsoler@gmail.com>
2025-03-29 13:40:37 +0000

    improving the about template

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,6 @@
 """defines url patterns for accoutns"""
 
-from django.urls import path,include
-from django.views.generic.base import TemplateView
+from django.urls import path
 from . import views
 
 
--- between/urls.py
+++ between/urls.py
@@ -3,10 +3,10 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib.flatpages import views
-from django.contrib.flatpages.sitemaps import FlatPageSitemap
 from django.contrib.sitemaps.views import sitemap
 from decouple import config
 from .sitemaps import StaticViewSitemap
+from django.views.generic.base import TemplateView
 
 sitemaps = {
     "static": StaticViewSitemap,
@@ -26,7 +26,8 @@ urlpatterns = [
     path("articles/", include("django.contrib.flatpages.urls")), #group of flat pages, access by 
     path("about/", views.flatpage, {"url": "/about/"}, name="about"),#To add custom url of flat pages
 
-    path("__reload__/", include("django_browser_reload.urls")), # for reload
+    path("__reload__/", include("django_browser_reload.urls")), # for dj reload app
+    path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type='text/plain')), #robots txt
 
     #adding a site map
     path("sitemap.xml/",
