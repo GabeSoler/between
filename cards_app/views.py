bbb209553b402abdef6a214ce6a3039a35efad10
GabeSoler <gsoler@gmail.com>
2026-04-15 09:34:54 +0100

    rendering basic cards pipe

--- cards_app/views.py
+++ cards_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from .models import Card
 # Create your views here.
 
 def card_list(request):
-    return render(request, 'cards_app/card_list.html')
+    cards = Card.objects.all()
+    context = {'cards': cards}
+    return render(request, 'cards_app/card_list.html',context)
