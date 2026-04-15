88652e506df4dc314768ac8944bc087c12b3bcd0
GabeSoler <gsoler@gmail.com>
2026-04-15 09:37:51 +0100

    cards rendered with basic style

--- cards_app/views.py
+++ cards_app/views.py
@@ -3,6 +3,6 @@ from .models import Card
 # Create your views here.
 
 def card_list(request):
-    cards = Card.objects.all()
+    cards = Card.objects.all().select_related('card_type')
     context = {'cards': cards}
     return render(request, 'cards_app/card_list.html',context)
