c9c019d1652baeb8da09904dd3accfef2c07e2f7
GabeSoler <gsoler@gmail.com>
2026-04-15 09:31:48 +0100

    card view and url

--- between/urls.py
+++ between/urls.py
@@ -21,7 +21,8 @@ urlpatterns = [
     path('techniques/',include('techniques_app.urls')),
     path('diver/',include('dive_app.urls')),
     path('',include('between_app.urls')),
-    
+    path('',include('cards_app.urls')),
+
     #flat pages config
     path("articles/", include("django.contrib.flatpages.urls")), #group of flat pages, access by 
     path("about/", views.flatpage, {"url": "/about/"}, name="about"),#To add custom url of flat pages
--- cards_app/views.py
+++ cards_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def card_list(request):
+    return render(request, 'cards_app/cards_list.html')
