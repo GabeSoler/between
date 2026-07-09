009994cc2b1976f6c38487e2e5b77a6cd63a23d2
GabeSoler <gsoler@gmail.com>
2026-07-09 23:46:38 +0100

    urls little / error

--- between/urls.py
+++ between/urls.py
@@ -1,42 +1,47 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls.static import static
+from decouple import config
 from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
 from django.contrib.flatpages import views
 from django.contrib.sitemaps.views import sitemap
-from decouple import config
-from .sitemaps import StaticViewSitemap
+from django.urls import include, path
 from django.views.generic.base import TemplateView
+
+from .sitemaps import StaticViewSitemap
 
 sitemaps = {
     "static": StaticViewSitemap,
 }
 
-urlpatterns = [
-    path(config('ADMIN_URL'), admin.site.urls),
-    path('accounts/',include('allauth.urls')),
-    path('accounts/',include('accounts.urls')),
-
-    path('learning/',include('learning_logs.urls')),
-    path('techniques/',include('techniques_app.urls')),
-    path('diver/',include('dive_app.urls')),
-    path('',include('between_app.urls')),
-    path('cards',include('cards_app.urls')),
-    path('companion',include('companion_app.urls')),
-
-    #flat pages config
-    path("articles/", include("django.contrib.flatpages.urls")), #group of flat pages, access by 
-    path("about/", views.flatpage, {"url": "/about/"}, name="about"),#To add custom url of flat pages
-    path("data-policy/", views.flatpage, {"url": "/data-policy/"}, name="data_policy"),#To add custom url of flat pages
-
-    path("__reload__/", include("django_browser_reload.urls")), # for dj reload app
-    path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type='text/plain')), #robots txt
-
-    #adding a site map
-    path("sitemap.xml/",
-         sitemap,
-        {"sitemaps":sitemaps},
-        name="django.contrib.sitemaps.views.sitemap",
+urlpatterns = (
+    [
+        path(config("ADMIN_URL"), admin.site.urls),
+        path("accounts/", include("allauth.urls")),
+        path("accounts/", include("accounts.urls")),
+        path("learning/", include("learning_logs.urls")),
+        path("techniques/", include("techniques_app.urls")),
+        path("diver/", include("dive_app.urls")),
+        path("", include("between_app.urls")),
+        path("cards/", include("cards_app.urls")),
+        path("companion", include("companion_app.urls")),
+        # flat pages config
+        path("articles/", include("django.contrib.flatpages.urls")),  # group of flat pages, access by
+        path("about/", views.flatpage, {"url": "/about/"}, name="about"),  # To add custom url of flat pages
+        path(
+            "data-policy/", views.flatpage, {"url": "/data-policy/"}, name="data_policy"
+        ),  # To add custom url of flat pages
+        path("__reload__/", include("django_browser_reload.urls")),  # for dj reload app
+        path(
+            "robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")
+        ),  # robots txt
+        # adding a site map
+        path(
+            "sitemap.xml/",
+            sitemap,
+            {"sitemaps": sitemaps},
+            name="django.contrib.sitemaps.views.sitemap",
         ),
-
-    ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    ]
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
